72b1d498ce4edf1bfa2f853dfcc5ad2318a0fe6b
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-06-19 20:02:24 +0200

    GLUT.NET started with MatrixStack
    This is an OpenGL Utility Toolkit for .NET applications.  This is NOT a
    "port" from the original GLUT, but I do not have anything else to call
    it.  ObjGLUF was not entirely appropriate

---
+++ GLUT.NET/Core/MatrixStack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Pencil.Gaming.MathUtils;
+
+namespace GLUT.NET.Core
+{
+    /// <summary>
+    /// MatrixStack is a convenient matrix stack which can easily combine
+    /// transformations of a hierarchal scene while only computing
+    /// each matrix multiplication once
+    /// </summary>
+    public class MatrixStack
+    {
+        Stack<Matrix> Stack = new Stack<Matrix>();
+        public void Push(Matrix mat)
+        {
+            if (Stack.Count != 0)
+            {
+                Matrix newMat = mat * Stack.Peek();
+                Stack.Push(newMat);
+            }
+            else
+            {
+                Stack.Push(mat);
+            }
+        }
+        public void Pop()
+        {
+            Stack.Pop();
+        }
+        public void Peek()
+        {
+            Stack.Peek();
+        }
+        public int Count()
+        {
+            return Stack.Count;
+        }
+        bool Empty()
+        {
+            return Stack.Count == 0;
+        }
+    }
+}
